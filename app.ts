6fe7cdaf89db5bf68f4c17c4b4662bd896174a04
Дюша <58334576+Menxy@users.noreply.github.com>
2023-04-10 04:58:30 +0300

    Typescript: First

--- app.ts
+++ app.ts
@@ -2,3 +2,12 @@ const nope = (str:string[]) => {
   str.push('1')
 }
 nope(['2'])
+
+interface IUser {
+  name:string;
+  age:number;
+  status: boolean
+}
+
+const user: IUser = {name:'Nope',age:12,status:true}
+console.log(user)
